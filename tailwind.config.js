f65db647deee47e94cbf6a43af53339376979da4
Aleksandr Zapparov <alexander@zapparov.dev>
2024-02-19 08:37:44 +0100

    project chore bump packages and cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/**/*.njk', './src/**/*.md',],
+  content: ['./src/**/*.njk', './src/**/*.md'],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: "#3366CC",
+            },
+          },
+        },
+      },
+      borderRadius: {
+        xs: "4px",
+        sm: "6px",
+        DEFAULT: "10px",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        inherit: "inherit",
+        white: "#fff",
+
+      },
+      aspectRatio: {
+        auto: "auto",
+        box: "1",
+        landscape: "4/3",
+        portrait: "3/4",
+        video: "16/9",
+      },
+      maxWidth: {
+        "copy-xs": "25ch",
+        "copy-sm": "45ch",
+        "copy-md": "55ch",
+        "copy-lg": "65ch",
+        "copy-xl": "75ch",
+        "copy-2xl": "85ch",
+      },
+    },
   },
   plugins: [
-    require("daisyui")
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwindcss-safe-area"),
+    ({ addComponents, theme }) => {
+      addComponents({
+        ".container": {
+          "@apply px-4 max-w-screen-2xl mx-auto": {},
+        },
+        ".prose": {
+          "@apply max-w-4xl mx-auto": {},
+        },
+      });
+    },
   ],
-  daisyui: {
-    styled: true,
-    themes: ["retro", "dracula"],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: "",
-    darkTheme: "dracula"
-  }
-}
+};
