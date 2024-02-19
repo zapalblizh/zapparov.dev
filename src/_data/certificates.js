90da0802741720636933207d9832705ae305d208
Aleksandr Zapparov <alexander@zapparov.dev>
2024-02-19 10:58:31 +0100

    Update Certificates Data

--- src/_data/certificates.js
+++ src/_data/certificates.js
@@ -1,130 +1,137 @@
 const certificates = [
-    {
-        name: "Introduction to Cloud Computing",
-        issuer: "IBM",
-        issueDate: "2022-11-04",
-        link: "https://coursera.org/verify/ALHQX8TGVPGD",
-        img: "Introduction-to-Cloud-Computing.jpeg"
-    }, {
-        name: "Introduction to Web Development with HTML, CSS, JavaScript",
-        issuer: "IBM",
-        issueDate: "2022-11-11",
-        link: "https://coursera.org/verify/DY3FCJ39SZD8",
-        img: "Introduction-to-Web-Development-with-HTML-CSS-and-Javascript.jpeg"
-    }, {
-        name: "Getting Started with Git and GitHub",
-        issuer: "IBM",
-        issueDate: "2022-11-13",
-        link: "https://coursera.org/verify/58BJMM7489HV",
-        img: "Getting-Started-with-Git-and-Github.jpeg"
-    }, {
-        name: "Developing Front-End Apps with React",
-        issuer: "IBM",
-        issueDate: "2022-12-20",
-        link: "https://coursera.org/verify/278Y5MZ9ASMJ",
-        img: "Developing-Front-End-Apps-with-React.jpeg"
-    }, {
-        name: "Developing Back-End Apps with Node.js and Express",
-        issuer: "IBM",
-        issueDate: "2022-12-31",
-        link: "https://coursera.org/verify/454XTFB7AK6X",
-        img: "Developing-Back-End-Apps-with-NodeJS-and-Express.jpeg"
-    }, {
-        name: "Python for Data Science, AI & Development",
-        issuer: "IBM",
-        issueDate: "2022-12-12",
-        link: "https://coursera.org/verify/UQ83UEEWREXD",
-        img: "Python-for-Data-Science-AI-and-Development.jpeg"
-    }, {
-        name: "Python Project for AI & Application Development",
-        issuer: "IBM",
-        issueDate: "2023-01-18",
-        link: "https://coursera.org/verify/LCM6ZXBQRJNH",
-        img: "Python-Project-for-AI-and-Application-Development.jpeg"
-    }, {
-        name: "Developing Applications with SQL, Databases, and Django",
-        issuer: "IBM",
-        issueDate: "2023-02-09",
-        link: "https://coursera.org/verify/9GBU9S9ZUQK9",
-        img: "Developing-Applications-with-SQL-Databases-and-Django.jpeg"
-    }, {
-        name: "Introduction to Containers w/ Docker, Kubernetes & OpenShift",
-        issuer: "IBM",
-        issueDate: "2022-12-05",
-        link: "https://coursera.org/verify/779EV4HCHZJV",
-        img: "Introduction-to-Containers-w-Docker-Kubernetes-and-OpenShift.jpeg"
-    }, {
-        name: "Application Development using Microservices and Serverless",
-        issuer: "IBM",
-        issueDate: "2023-02-22",
-        link: "https://coursera.org/verify/NXM2KT9E87DJ",
-        img: "Application-Development-using-Microservices-and-Serverless.jpeg"
-    }, {
-        name: "Full Stack Cloud Development Capstone Project",
-        issuer: "IBM",
-        issueDate: "2023-05-24",
-        link: "https://coursera.org/verify/EW6K9LSW5UV3",
-        img: "Full-Stack-Cloud-Development-Capstone-Project.jpeg"
-    }, {
-        name: "Full Stack Software Developer Assessment",
-        issuer: "IBM",
-        issueDate: "2023-06-02",
-        link: "https://coursera.org/verify/D966TT5D4NFT",
-        img: "Full-Stack-Software-Developer-Assessment.jpeg"
-    }, {
-        name: "Cloud Application Development Foundations",
-        issuer: "IBM",
-        issueDate: "2022-12-04",
-        link: "https://www.coursera.org/account/accomplishments/specialization/KZKJWSNU93VN",
-        img: "Cloud-Application-Development-Foundations.png"
-    }, {
-        name: "DELE Level B2",
-        issuer: "Instituto Cervantes",
-        issueDate: "2022-10-13",
-        link: "",
-        img: "DELE_B2.png"
-    }, {
-        name: "Pearson EDEXCEL International GCSE",
-        issuer: "Pearson EDEXCEL",
-        issueDate: "2023-09-30",
-        link: "",
-        img: "British-Council-GCSE-2022.png"
-    }, {
-        name: "Pearson EDEXCEL International As Levels Physics",
-        issuer: "Pearson EDEXCEL",
-        issueDate: "2023-03-08",
-        link: "",
-        img: "As-level-Pearson-Physics.png"
-    }, {
-        name: "CIE As Levels Maths and English Language",
-        issuer: "CIE",
-        issueDate: "2023-11-01",
-        link: "",
-        img: "As-levels-CIE.png"
-    }, {
-        name: "IELTS English",
-        issuer: "CIE",
-        issueDate: "2022-03-03",
-        link: "",
-        img: "IELTS.png"
-    }, {
-        name: "HTML, CSS and Javascript for Web Developers",
-        issuer: "Johns Hopkins University",
-        issueDate: "2023-06-29",
-        link: "https://www.coursera.org/account/accomplishments/verify/K853LNH9NCM3",
-        img: "HTML-CSS-and-Javascript-for-Web-Developers.png",
-    }, {
-        name: "Program your own mobile App",
-        issuer: "La Salle University Ramon Llull",
-        issueDate: "2023-07-14",
-        link: "",
-        img: "LaSalle-certificate-summer-camp.png",
-    }
-
+  {
+    name: "Introduction to Cloud Computing",
+    issuer: "IBM",
+    issueDate: "2022-11-04",
+    link: "https://coursera.org/verify/ALHQX8TGVPGD",
+    img: "Introduction-to-Cloud-Computing.jpeg"
+  }, {
+    name: "Introduction to Web Development with HTML, CSS, JavaScript",
+    issuer: "IBM",
+    issueDate: "2022-11-11",
+    link: "https://coursera.org/verify/DY3FCJ39SZD8",
+    img: "Introduction-to-Web-Development-with-HTML-CSS-and-Javascript.jpeg"
+  }, {
+    name: "Getting Started with Git and GitHub",
+    issuer: "IBM",
+    issueDate: "2022-11-13",
+    link: "https://coursera.org/verify/58BJMM7489HV",
+    img: "Getting-Started-with-Git-and-Github.jpeg"
+  }, {
+    name: "Developing Front-End Apps with React",
+    issuer: "IBM",
+    issueDate: "2022-12-20",
+    link: "https://coursera.org/verify/278Y5MZ9ASMJ",
+    img: "Developing-Front-End-Apps-with-React.jpeg"
+  }, {
+    name: "Developing Back-End Apps with Node.js and Express",
+    issuer: "IBM",
+    issueDate: "2022-12-31",
+    link: "https://coursera.org/verify/454XTFB7AK6X",
+    img: "Developing-Back-End-Apps-with-NodeJS-and-Express.jpeg"
+  }, {
+    name: "Python for Data Science, AI & Development",
+    issuer: "IBM",
+    issueDate: "2022-12-12",
+    link: "https://coursera.org/verify/UQ83UEEWREXD",
+    img: "Python-for-Data-Science-AI-and-Development.jpeg"
+  }, {
+    name: "Python Project for AI & Application Development",
+    issuer: "IBM",
+    issueDate: "2023-01-18",
+    link: "https://coursera.org/verify/LCM6ZXBQRJNH",
+    img: "Python-Project-for-AI-and-Application-Development.jpeg"
+  }, {
+    name: "Developing Applications with SQL, Databases, and Django",
+    issuer: "IBM",
+    issueDate: "2023-02-09",
+    link: "https://coursera.org/verify/9GBU9S9ZUQK9",
+    img: "Developing-Applications-with-SQL-Databases-and-Django.jpeg"
+  }, {
+    name: "Introduction to Containers w/ Docker, Kubernetes & OpenShift",
+    issuer: "IBM",
+    issueDate: "2022-12-05",
+    link: "https://coursera.org/verify/779EV4HCHZJV",
+    img: "Introduction-to-Containers-w-Docker-Kubernetes-and-OpenShift.jpeg"
+  }, {
+    name: "Application Development using Microservices and Serverless",
+    issuer: "IBM",
+    issueDate: "2023-02-22",
+    link: "https://coursera.org/verify/NXM2KT9E87DJ",
+    img: "Application-Development-using-Microservices-and-Serverless.jpeg"
+  }, {
+    name: "Full Stack Cloud Development Capstone Project",
+    issuer: "IBM",
+    issueDate: "2023-05-24",
+    link: "https://coursera.org/verify/EW6K9LSW5UV3",
+    img: "Full-Stack-Cloud-Development-Capstone-Project.jpeg"
+  }, {
+    name: "Full Stack Software Developer Assessment",
+    issuer: "IBM",
+    issueDate: "2023-06-02",
+    link: "https://coursera.org/verify/D966TT5D4NFT",
+    img: "Full-Stack-Software-Developer-Assessment.jpeg"
+  }, {
+    name: "Cloud Application Development Foundations",
+    issuer: "IBM",
+    issueDate: "2022-12-04",
+    link: "https://www.coursera.org/account/accomplishments/specialization/KZKJWSNU93VN",
+    img: "Cloud-Application-Development-Foundations.png"
+  }, {
+    name: "DELE Level B2",
+    issuer: "Instituto Cervantes",
+    issueDate: "2022-10-13",
+    link: "",
+    img: "DELE_B2.png"
+  }, {
+    name: "Pearson EDEXCEL International GCSE",
+    issuer: "Pearson EDEXCEL",
+    issueDate: "2023-09-30",
+    link: "",
+    img: "British-Council-GCSE-2022.png"
+  }, {
+    name: "Pearson EDEXCEL International As Levels Physics",
+    issuer: "Pearson EDEXCEL",
+    issueDate: "2023-03-08",
+    link: "",
+    img: "As-level-Pearson-Physics.png"
+  }, {
+    name: "CIE As Levels Maths and English Language",
+    issuer: "CIE",
+    issueDate: "2023-11-01",
+    link: "",
+    img: "As-levels-CIE.png"
+  }, {
+    name: "IELTS English",
+    issuer: "CIE",
+    issueDate: "2022-03-03",
+    link: "",
+    img: "IELTS.png"
+  }, {
+    name: "HTML, CSS and Javascript for Web Developers",
+    issuer: "Johns Hopkins University",
+    issueDate: "2023-06-29",
+    link: "https://www.coursera.org/account/accomplishments/verify/K853LNH9NCM3",
+    img: "HTML-CSS-and-Javascript-for-Web-Developers.png",
+  }, {
+    name: "Program your own mobile App",
+    issuer: "La Salle University Ramon Llull",
+    issueDate: "2023-07-14",
+    link: "",
+    img: "LaSalle-certificate-summer-camp.png",
+  }, {
+    name: "Hubspot CMS for Developers I",
+    issuer: "Hubspot Academy",
+    issueDate: "2024-01-25",
+    link: "https://app.hubspot.com/academy/achievements/d1czztr9/en/1/aleksandr-zapparov/hubspot-cms-for-developers",
+    img: "Hubspot-CMS-for-Developers-I-Certificate.png",
+  }, {
+    name: "Hubspot CMS for Developers II",
+    issuer: "Hubspot Academy",
+    issueDate: "2024-02-15",
+    link: "https://app.hubspot.com/academy/achievements/8yqzk7ct/en/1/aleksandr-zapparov/hubspot-cms-for-developers-ii-best-practices",
+    img: "Hubspot-CMS-for-Developers-II-Certificate.png",
+  }
 ];
 
 module.exports = certificates;
-
-
-
-
